a07b5705bd6fdc6d525e25efb215a516b7b4b190
Leo Igane <iganeleo@gmail.com>
2018-06-09 00:48:53 +0300

    tests the save function

--- Password-Lock_test.py
+++ Password-Lock_test.py
@@ -32,3 +32,11 @@ class TestContact(unittest.TestCase):
         self.assertEqual(self.new_contact.last_name,"Muriuki")
         self.assertEqual(self.new_contact.number,"0712345678")
         self.assertEqual(self.new_contact.email,"james@ms.com")
+
+    def test_save_contact(self):
+        '''
+        test_save_contact test case to test if the contact object is saved into
+         the contact list
+        '''
+        self.new_contact.save_contact() # saving the new contact
+        self.assertEqual(len(Contact.contact_list),1)
