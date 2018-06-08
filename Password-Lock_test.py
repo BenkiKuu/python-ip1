64fc417547b36f672611a97db580dc892061e3c1
Leo Igane <iganeleo@gmail.com>
2018-06-09 00:42:39 +0300

    creates the test up and teardown test

--- Password-Lock_test.py
+++ Password-Lock_test.py
@@ -11,3 +11,14 @@ class TestContact(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
     # Items up here .......
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        Contact.contact_list = []
