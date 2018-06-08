4fefd0bc5931a4052b828d3eed8393c331bcdbb3
Leo Igane <iganeleo@gmail.com>
2018-06-09 00:40:52 +0300

    creates the test class

--- Password-Lock_test.py
+++ Password-Lock_test.py
@@ -1,3 +1,13 @@
  import unittest  # Importing the unittest module
  from contact import Contact # Importing the contact class
  import pyperclip
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    # Items up here .......
